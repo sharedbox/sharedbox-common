14034e4d92a364ed6d02aa2516488c7b0ab0a7d0
Rafael Costi <rafaelcosti@outlook.com>
2021-05-04 11:00:14 -0300

    New function on DateUtils

--- src/main/java/br/com/sharedbox/common/utils/DateTimeUtils.java
+++ src/main/java/br/com/sharedbox/common/utils/DateTimeUtils.java
@@ -1,10 +1,26 @@
 package br.com.sharedbox.common.utils;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 /**
  * Date time utils
  * @author Rafael Costi <rafaelcosti@outlook.com>
  * @version 1.0.0
+ * @since 05/01/2021
  */
 public class DateTimeUtils {
-
+	/**
+	 * <p>
+	 * Convert String format MMMM/yyyy to LocalDate
+	 * </p>
+	 *  
+	 * @param dateStr
+	 * @return date
+	 */
+	public static LocalDate stringMMMMyyyyToLocalDate(String date) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MMMM/yyyy");
+		LocalDate ld = LocalDate.parse("01/" + date, formatter);
+		return ld;
+	}
 }
