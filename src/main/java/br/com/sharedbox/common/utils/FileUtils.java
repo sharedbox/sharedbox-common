bab40c5f8a41e9c8457be51b17f8142f87f67e34
Rafael Costi <rafaelcosti@outlook.com>
2021-08-24 10:10:32 -0300

    Add new function write on FileUtils

--- src/main/java/br/com/sharedbox/common/utils/FileUtils.java
+++ src/main/java/br/com/sharedbox/common/utils/FileUtils.java
@@ -1,7 +1,10 @@
 package br.com.sharedbox.common.utils;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -43,5 +46,38 @@ public class FileUtils extends org.apache.commons.io.IOUtils {
 		List<String> lines = new ArrayList<String>();
 		lines = Files.readAllLines(path, StandardCharsets.UTF_8);
 		return lines.toArray(new String[lines.size()]);
+	}
+
+	/**
+	 * 
+	 * @param value
+	 * @param path
+	 * @throws IOException
+	 */
+	public static void write(String value, String path) throws IOException {
+		write(value, path, "UTF-8");
+	}
+
+	/**
+	 * 
+	 * @param value
+	 * @param path
+	 * @param charset
+	 * @throws IOException
+	 */
+	public static void write(String value, String path, String charset) throws IOException {
+		write(value, path, Charset.forName(charset));
+	}
+	
+	/**
+	 * 
+	 * @param jsonCountries
+	 * @param path
+	 * @param charset
+	 * @throws IOException
+	 */
+	public static void write(String value, String path, Charset charset) throws IOException {
+		OutputStream ops = new FileOutputStream(path);
+		write(value, ops, charset);
 	}
 }
--- src/test/java/br/com/sharedbox/common/utils/test/FileUtilsTest.java
+++ src/test/java/br/com/sharedbox/common/utils/test/FileUtilsTest.java
@@ -1,5 +1,6 @@
 package br.com.sharedbox.common.utils.test;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.io.IOException;
@@ -31,5 +32,16 @@ public class FileUtilsTest {
 	@Test
 	void readAllLinesTest() throws IOException {
 		assertNotNull(FileUtils.readAllLines("D:\\SharedBox\\test\\test.txt"));
+	}
+
+	/**
+	 * 
+	 * @throws IOException 
+	 */
+	@Test
+	void writeTest() throws IOException{
+		String value = "testWrite";
+		String path = "D:\\SharedBox\\test\\testWrite.txt";
+		assertDoesNotThrow(() -> FileUtils.write(value, path));
 	}
 }
