a2a6174f04e97ce517b01dedd2da52574d2f88ee
Rafael Costi <rafaelcosti@outlook.com>
2022-01-06 10:08:49 -0300

    implement JWT class with HMAC256 algorithm

--- src/main/java/br/com/sharedbox/common/security/Jwt.java
+++ src/main/java/br/com/sharedbox/common/security/Jwt.java
@@ -1,10 +1,71 @@
 package br.com.sharedbox.common.security;
 
+import java.util.Date;
+import java.util.Map;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTCreator.Builder;
+
 /**
  * This class contain write and read jwt token
  * @author Rafael Costi <rafaelcosti@outlook.com>
  * @version 1.0.0
  */
 public class Jwt {
-
+	/**
+	 * This method generate JWT token 
+	 * 
+	 * @param secret
+	 * @param subject
+	 * @param expiration
+	 * @param algorithm
+	 * @return token
+	 */
+	public static String generateToken(String secret, String subject, long expiration, JwtAlgorithim algorithm) {
+		return generateToken(secret, subject, expiration, algorithm, null) ;
+	}
+	
+	/**
+	 * This method generate JWT token with claims
+	 * 
+	 * @param secret
+	 * @param subject
+	 * @param expiration
+	 * @param algorithm
+	 * @param claims
+	 * @return token
+	 */
+	public static String generateToken(String secret, String subject, long expiration, JwtAlgorithim algorithm, Map<String, String> claims) {
+		return generateToken(secret, subject, expiration, algorithm, claims, null);
+	}
+	
+	/**
+	 * This method generate JWT token with claims and header claims
+	 * 
+	 * @param secret
+	 * @param subject
+	 * @param expiration
+	 * @param algorithm
+	 * @param claims
+	 * @param headerClaims
+	 * @return token
+	 */
+	public static String generateToken(String secret, String subject, long expiration, JwtAlgorithim algorithm
+			, Map<String, String> claims, Map<String, Object> headerClaims) {
+		Builder builder = JWT.create()
+					.withSubject(subject)
+					.withExpiresAt(new Date(System.currentTimeMillis() + expiration));
+		
+		if (claims != null) {
+			for (String key : claims.keySet()) {
+				builder = builder.withClaim(key, claims.get(key));
+			}
+		}
+		
+		if (headerClaims != null) {
+			builder = builder.withHeader(headerClaims);
+		}
+			
+		return builder.sign(algorithm.getAlgorithim(secret));
+	}
 }
--- src/test/java/br/com/sharedbox/common/security/test/JwtTest.java
+++ src/test/java/br/com/sharedbox/common/security/test/JwtTest.java
@@ -1,10 +1,16 @@
 package br.com.sharedbox.common.security.test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
 import br.com.sharedbox.common.security.Jwt;
+import br.com.sharedbox.common.security.JwtAlgorithim;
 
 /**
  * Call test of Jwt class
@@ -20,5 +26,42 @@ public class JwtTest {
 	@Test
 	public void construcotTest() {
 		assertNotNull(new Jwt());
+	}
+	
+	/**
+	 * Generate token test
+	 */
+	@Test
+	public void generateTokenTest() {
+		String subject = "SharedBox";
+		
+		String token = Jwt.generateToken("SECRET_TEST", subject, 6000, JwtAlgorithim.HS512);
+		assertNotNull(token);
+		
+		Map<String, String> claims = new HashMap<String, String>();
+		claims.put("user", "sharedbox");
+		claims.put("test", "test");
+
+		Map<String, Object> claimsHeader = new HashMap<String, Object>();
+		claimsHeader.put("system", "sharedbox");
+		
+		token = Jwt.generateToken("SECRET_TEST", subject, 6000, JwtAlgorithim.HS512, claims);
+		assertNotNull(token);
+		
+		token = Jwt.generateToken(null, subject, 6000, JwtAlgorithim.NONE, claims);
+		assertNotNull(token);
+		
+		token = Jwt.generateToken("SECRET_TEST", subject, 6000, JwtAlgorithim.HS512, null, claimsHeader);
+		
+		IllegalArgumentException thrownIllegalArgumentException = assertThrows(IllegalArgumentException.class, 
+				() -> Jwt.generateToken(null, subject, 6000, JwtAlgorithim.HS512));
+		assertTrue(thrownIllegalArgumentException.getMessage().contains("JWT invalid secret"));
+
+		token = Jwt.generateToken("SECRET_TEST", subject, 6000, JwtAlgorithim.HS512, claims, claimsHeader);
+		assertNotNull(token);
+		token = Jwt.generateToken("SECRET_TEST", subject, 6000, JwtAlgorithim.HS384, claims, claimsHeader);
+		assertNotNull(token);
+		token = Jwt.generateToken("SECRET_TEST", subject, 6000, JwtAlgorithim.HS256, claims, claimsHeader);
+		assertNotNull(token);
 	}
 }
